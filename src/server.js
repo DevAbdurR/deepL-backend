580abf762eb28b2aa6bbb4cb964a006f6a80f893
Abdul Rehman <abdurrehmandev6@gmail.com>
2024-09-30 04:50:47 +0500

    env added

--- src/server.js
+++ src/server.js
@@ -52,6 +52,12 @@ app.post("/translate", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
